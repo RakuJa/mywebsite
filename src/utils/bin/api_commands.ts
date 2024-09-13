c04a2763a7c681e81d695f225cd02b55ebd81f0b
RakuJa <github@danielegiachetto.com>
2024-09-13 14:05:40 +0200

    Update info, add quotes (#5)

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -4,7 +4,7 @@ import { getProjects } from '../api';
 import { getReadme } from '../api';
 import { getWeather } from '../api';
 
-export const projects = async (args: string[]): Promise<string> => {
+export const list_github_repos = async (args: string[]): Promise<string> => {
   const projects = await getProjects();
   return projects
     .map(
